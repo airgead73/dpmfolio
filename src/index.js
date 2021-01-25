8a2cbf16a9fd2df2e9a54ef9dfaea4b636433894
Dan Moneypenny <catullus1973@gmail.com>
2021-01-25 10:04:46 -0800

    adjust index

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 
 class App extends React.Component {
   render() {
-    return <div><h1>dpmfolio</h1></div>
+    return <div><h1>dpmfolio react</h1></div>
   }
 }
 
