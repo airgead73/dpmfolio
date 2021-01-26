61530ef55f61ad3b935c15de9692a1f03fac122c
Dan Moneypenny <catullus1973@gmail.com>
2021-01-26 15:29:03 -0800

    test

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React from 'react';
 
 function App() {
+
+  const title = 'Welcome to the new blog';
+  const likes = 50
+
   return (
-    <div>
-      dpmfolio
+    <div className="App">
+      <div className="content">
+        <h1>{ title }</h1>
+        <p>liked { likes } times</p>
+      </div>      
     </div>
   )
 }
