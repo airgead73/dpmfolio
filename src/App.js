b2ff259d7437e6941c1bdbc6dc824380b7860d22
Dan Moneypenny <catullus1973@gmail.com>
2021-01-26 15:32:33 -0800

    test 2

--- src/App.js
+++ src/App.js
@@ -2,14 +2,10 @@ import React from 'react';
 
 function App() {
 
-  const title = 'Welcome to the new blog';
-  const likes = 50
-
   return (
     <div className="App">
       <div className="content">
-        <h1>{ title }</h1>
-        <p>liked { likes } times</p>
+        <h1>App Component</h1>
       </div>      
     </div>
   )
